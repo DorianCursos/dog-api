afcbbe37962d5196a1c564828b5c4dcc46cb47b9
Dorian <doriancursos@gmail.com>
2024-07-17 09:56:43 +0200

    updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,28 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const breedsSelect = document.getElementById('breeds');
+
+const printAllBreeds = breeds => {
+  const fragment = document.createDocumentFragment();
+  breeds.forEach(breed => {
+    const newOption = document.createElement('option');
+    newOption.textContent = breed;
+    fragment.append(newOption);
+  });
+
+  breedsSelect.append(fragment);
+};
+
+const getAllBreeds = async () => {
+  try {
+    const response = await fetch('https://dog.ceo/api/breeds/list/all');
+    const data = await response.json();
+    const allBreeds = Object.keys(data.message);
+    printAllBreeds(allBreeds);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getAllBreeds();
